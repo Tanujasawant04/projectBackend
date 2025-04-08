27c7d48569015d9a5913c3b693e8d1ac11e5e5b5
Tanujasawant04 <tanujasawant2004@gmail.com>
2025-04-08 12:12:02 +0530

    hi

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use(morgan('dev'))
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/rahulEcomm')
+
+mongoose.connect('mongodb+srv://sawanttanuja12:me6a9iRpF8ZlqSsy@cluster0.nfpkwuv.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>{
     console.log("db is connected")
 }).catch((error)=>{
